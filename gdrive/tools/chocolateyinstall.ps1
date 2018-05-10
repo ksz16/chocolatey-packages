9dc8f46b3cd75f4a86774bc0f4536a2d71d03d69
unknown <ksz_16@yahoo.com>
2018-05-10 19:11:01 +0200

    gdrive 2.1.0

---
+++ gdrive/tools/chocolateyinstall.ps1
@@ -0,0 +1,7 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
+
+$installerFile = if ((Get-OSArchitectureWidth 64) -and $env:chocolateyForceX86 -ne 'true') {
+         Write-Host "Installing x64 bit version"; Remove-Item $toolsPath\gdrive-windows-386.exe; Rename-Item -Path $toolsPath\gdrive-windows-x64.exe -NewName "gdrive.exe"
+} else { Write-Host "Installing x32 bit version"; Remove-Item $toolsPath\gdrive-windows-x64.exe; Rename-Item -Path $toolsPath\gdrive-windows-386.exe -NewName "gdrive.exe" }
