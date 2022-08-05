aef0937b6b0579eb4157c5cba8d93d277d02d7e4
Amber Schenck <amber.schenck@ease.com>
2022-08-05 13:32:51 -0700

    Update gdrive to 2.1.1 for --service-account support

--- gdrive/tools/chocolateyinstall.ps1
+++ gdrive/tools/chocolateyinstall.ps1
@@ -2,6 +2,14 @@
 
 $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 
+Remove-Item $toolsPath\gdrive.exe;
+
 $installerFile = if ((Get-OSArchitectureWidth 64) -and $env:chocolateyForceX86 -ne 'true') {
-         Write-Host "Installing x64 bit version"; Remove-Item $toolsPath\gdrive-windows-386.exe; Rename-Item -Path $toolsPath\gdrive-windows-x64.exe -NewName "gdrive.exe"
-} else { Write-Host "Installing x32 bit version"; Remove-Item $toolsPath\gdrive-windows-x64.exe; Rename-Item -Path $toolsPath\gdrive-windows-386.exe -NewName "gdrive.exe" }
+         Write-Host "Installing x64 bit version";
+	 Remove-Item $toolsPath\gdrive-windows-386.exe;
+	 Rename-Item -Path $toolsPath\gdrive-windows-x64.exe -NewName "gdrive.exe"
+} else {
+	 Write-Host "Installing x32 bit version";
+	 Remove-Item $toolsPath\gdrive-windows-x64.exe;
+	 Rename-Item -Path $toolsPath\gdrive-windows-386.exe -NewName "gdrive.exe"
+}
