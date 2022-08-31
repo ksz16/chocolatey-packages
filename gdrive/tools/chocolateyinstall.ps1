a0119600f4e389553cc1e929d5ee3b5cbb7afcc1
ksz16 <22708888+ksz16@users.noreply.github.com>
2022-08-31 19:24:05 +0200

    gdrive 2.1.1

--- gdrive/tools/chocolateyinstall.ps1
+++ gdrive/tools/chocolateyinstall.ps1
@@ -1,7 +1,17 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
+Install-ChocolateyZipPackage `
+  -PackageName 'gdrive' `
+  -Url 'https://github.com/prasmussen/gdrive/releases/download/2.1.1/gdrive_2.1.1_windows_386.tar.gz' `
+  -UnzipLocation "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)" `
+  -Url64bit 'https://github.com/prasmussen/gdrive/releases/download/2.1.1/gdrive_2.1.1_windows_amd64.tar.gz' `
+  -Checksum 'e03346f9ec72225872d0e32b41abf192' `
+  -ChecksumType 'md5' `
+  -Checksum64 '14226cb66d7f6c1b84a5be911ff193f6' `
+  -ChecksumType64 'md5'
 
-$installerFile = if ((Get-OSArchitectureWidth 64) -and $env:chocolateyForceX86 -ne 'true') {
-         Write-Host "Installing x64 bit version"; Remove-Item $toolsPath\gdrive-windows-386.exe; Rename-Item -Path $toolsPath\gdrive-windows-x64.exe -NewName "gdrive.exe"
-} else { Write-Host "Installing x32 bit version"; Remove-Item $toolsPath\gdrive-windows-x64.exe; Rename-Item -Path $toolsPath\gdrive-windows-386.exe -NewName "gdrive.exe" }
+Get-ChocolateyUnzip `
+  -FileFullPath "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)\\*.tar" `
+  -Destination "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+
+Remove-Item -Path "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)\\*.tar"
