5ee4b4c793e1194a091ef6e7d906841004de416c
ksz <22708888+ksz16@users.noreply.github.com>
2023-02-07 20:48:50 +0100

    eget 1.3.2

--- eget/tools/chocolateyinstall.ps1
+++ eget/tools/chocolateyinstall.ps1
@@ -4,10 +4,10 @@ Remove-Item -Path "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)\\ege
 
 Install-ChocolateyZipPackage `
   -PackageName 'eget' `
-  -Url 'https://github.com/zyedidia/eget/releases/download/v1.3.1/eget-1.3.1-windows_386.zip' `
   -UnzipLocation "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)" `
-  -Url64bit 'https://github.com/zyedidia/eget/releases/download/v1.3.1/eget-1.3.1-windows_amd64.zip' `
-  -Checksum '80e5b67e7524aa6f5b07a8e56279884bb0f184d03e0459e4e3fbc8d6d5586798' `
+  -Url 'https://github.com/zyedidia/eget/releases/download/v1.3.2/eget-1.3.2-windows_386.zip' `
+  -Checksum '08a909d769bdbac6e0cf808d893f11832d400a4b44679a0a7b6965b19eab717c' `
   -ChecksumType 'sha256' `
-  -Checksum64 'e0f01e674d127f43bd56e67b1f8d4f4563f3f644a04b8da505d3a9a573b68b26' `
+  -Url64 'https://github.com/zyedidia/eget/releases/download/v1.3.2/eget-1.3.2-windows_amd64.zip' `
+  -Checksum64 '1dae2ba129d8bec1e839543d4b0680bf207f61a473557dd5ea40ede60d42e2dc' `
   -ChecksumType64 'sha256'
